9dd0b31364cf66719296cd3b287b02fdf8abe55f
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2020-10-09 18:28:56 +0530

    Add files via upload

---
+++ Loop/TableByForloop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int i,a;
+	
+	for(int j=1;j<=10;i++)
+	{
+		printf("Enter the that you need Table ");
+	   scanf("%d",&a);
+	for(i=1; i<=10;i++)
+	{
+		printf("%d  X  %d = %d\n", a,i,a*i);
+	}
+}
+}
